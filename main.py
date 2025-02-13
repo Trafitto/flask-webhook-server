d226589543c473aed4e2f92333d3177410aee052
Trafitto <borchimarco@gmail.com>
2025-02-13 23:06:45 +0100

    fix server response

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ MAC_ADDRESS = os.getenv("MAC_ADDRESS")
 @app.route(f"/webhook/{HOOK_ID}", methods=["POST"])
 def webhook():
     # data = request.json
-    print(f"Received webhook")
+    print(f"Received wake up lan webhook")
     send_magic_packet(MAC_ADDRESS)
     
-    return 204, None
+    return {"status": "success"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
